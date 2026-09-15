4685ccf464f4de9dcfb557b0441cd821d45b7c29
TangeyPonchik <ronormav@gmail.com>
2026-09-15 19:09:52 +0300

    обновление

--- push.bat
+++ push.bat
@@ -1,9 +1,10 @@
 @echo off
+chcp 65001 >nul
 cd /d C:\FireLand\FireLand
-set /p msg="Что изменил? "
+set /p msg="Commit message: "
 git add .
 git commit -m "%msg%"
 git push
 echo.
-echo Готово! Netlify задеплоит за 10-20 секунд.
+echo Done! Netlify will deploy in 10-20 seconds.
 pause
